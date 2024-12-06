1ddf9d94690ab89a6408f2e303a38d9717dceef1
Smartbyme15 <laibakaimkhani2004@gmail.com>
2024-12-06 14:13:00 -0800

    Add files via upload

---
+++ chapter01/app.js
@@ -0,0 +1,36 @@
+// question no:01 
+// 1. Write a script to greet your website visitor using JS alert
+// box?
+alert("welcome to my website ");
+// question no:02 
+// 2. Write a script to display following message on your web page:?
+// document.write("that's a script show your web page ");
+// question no:3
+// Write a script to display following message on your web
+// page: (Hint : Use line break)?
+document.write(" I am using <br>");
+document.write("to break the line.");
+// question no:04
+// . Write a script to display following messages in sequence:
+document.write("Hello, i am here!<br>");
+document.write("i am glat to you.<br>");
+document.write("you can ask any question to me .<br>");
+document.write("Thanks for visiting!");
+// question no:05
+// 5. Generate the following message through browser’s
+// developer console:?
+console.log("cleck right button and than select inspect so you can open select console so you can see  my massage . ");
+// question no:06
+// 6. Make use of alerts in your new/existing HTML & CSS
+// project.?
+// answer (i will put in html)
+
+// question no:07
+// 7. Practice placement of <script></script> element in
+// following sections of your project in exercise 6:
+// a. Head (there is a answer of these question)
+
+
+
+
+
